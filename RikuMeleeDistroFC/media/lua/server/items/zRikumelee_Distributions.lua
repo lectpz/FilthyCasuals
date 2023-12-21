c6b6226114cd727f524d26e52603a5ee8c394ad6
lectpz <117684200+lectpz@users.noreply.github.com>
2023-12-20 19:50:20 -0500

    Add files via upload

--- RikuMeleeDistroFC/media/lua/server/items/zRikumelee_Distributions.lua
+++ RikuMeleeDistroFC/media/lua/server/items/zRikumelee_Distributions.lua
@@ -10,32 +10,30 @@ end
 
 local function preDistributionMerge()
 	table.insert(ProceduralDistributions.list.StoreShelfMedical.items, "Coldpack");
-	table.insert(ProceduralDistributions.list.StoreShelfMedical.items, 0.5);
+	table.insert(ProceduralDistributions.list.StoreShelfMedical.items, 5);
 	
 	table.insert(ProceduralDistributions.list.MedicalStorageDrugs.items, "Coldpack");
-	table.insert(ProceduralDistributions.list.MedicalStorageDrugs.items, 0.5);
+	table.insert(ProceduralDistributions.list.MedicalStorageDrugs.items, 2);
 	
 	table.insert(ProceduralDistributions.list.MedicalClinicDrugs.items, "Coldpack");
-	table.insert(ProceduralDistributions.list.MedicalClinicDrugs.items, 0.5);
+	table.insert(ProceduralDistributions.list.MedicalClinicDrugs.items, 2);
 	
 	table.insert(ProceduralDistributions.list.ArmyStorageMedical.items, "Coldpack");
-	table.insert(ProceduralDistributions.list.ArmyStorageMedical.items, 0.5);
+	table.insert(ProceduralDistributions.list.ArmyStorageMedical.items, 1);
 	
 	table.insert(ProceduralDistributions.list.MedicalStorageTools.items, "Coldpack");
-	table.insert(ProceduralDistributions.list.MedicalStorageTools.items, 0.5);
+	table.insert(ProceduralDistributions.list.MedicalStorageTools.items, 2);
 	
 	table.insert(ProceduralDistributions.list.MedicalClinicTools.items, "Coldpack");
-	table.insert(ProceduralDistributions.list.MedicalClinicTools.items, 0.5);
+	table.insert(ProceduralDistributions.list.MedicalClinicTools.items, 2);
 	
+	---------------------------------------------------------------------------------
 	table.insert(ProceduralDistributions.list.CampingStoreGear.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.CampingStoreGear.items, 0.00001);
 
 	table.insert(ProceduralDistributions.list.FireStorageTools.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.FireStorageTools.items, 0.0001);
 	
-	table.insert(ProceduralDistributions.list.GarageCarpentry.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.GarageCarpentry.items, 0.00001);
-
 	table.insert(ProceduralDistributions.list.GigamartTools.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.GigamartTools.items, 0.00001);
 
@@ -44,9 +42,6 @@ local function preDistributionMerge()
 
 	table.insert(ProceduralDistributions.list.MeleeWeapons.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.MeleeWeapons.items, 0.004);
-
-	table.insert(ProceduralDistributions.list.GarageTools.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.GarageTools.items, 0.00001);
 
 	table.insert(ProceduralDistributions.list.JanitorTools.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.JanitorTools.items, 0.00001);
@@ -57,14 +52,8 @@ local function preDistributionMerge()
 	table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 0.002);
 
-	table.insert(ProceduralDistributions.list.WardrobeChild.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.WardrobeChild.items, 0.0000001);
-
 	table.insert(ProceduralDistributions.list.WardrobeWoman.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.000001);
-
-	table.insert(ProceduralDistributions.list.Hobbies.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.Hobbies.items, 0.0000001);
 
 	table.insert(ProceduralDistributions.list.PawnShopKnives.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.PawnShopKnives.items, 0.001);
@@ -75,36 +64,31 @@ local function preDistributionMerge()
 	table.insert(ProceduralDistributions.list.BarCounterWeapon.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.BarCounterWeapon.items, 0.00001);
 
-	table.insert(ProceduralDistributions.list.MusicStoreBass.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.MusicStoreBass.items, 0.00001);
-
-	table.insert(ProceduralDistributions.list.GarageMetalwork.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.GarageMetalwork.items, 0.00001);
-	
-	table.insert(ProceduralDistributions.list.LivingRoomShelf.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.LivingRoomShelf.items, 0.000001);
-	
 	table.insert(ProceduralDistributions.list.BedroomDresser.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.0000001);
-
-	table.insert(ProceduralDistributions.list.GigamartHousewares.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.GigamartHousewares.items, 0.00001);
 
 	table.insert(ProceduralDistributions.list.ToolStoreTools.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.ToolStoreTools.items, 0.00001);
 
 	table.insert(ProceduralDistributions.list.GunStoreAmmunition.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.GunStoreAmmunition.items, 0.002);
+	table.insert(ProceduralDistributions.list.GunStoreAmmunition.items, 0.0001);
 
 	table.insert(ProceduralDistributions.list.WardrobeMan.items, "WeaponCache");
 	table.insert(ProceduralDistributions.list.WardrobeMan.items, 0.00001);
 
 	table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 0.002);
+	table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 0.001);
 	
 	table.insert(ProceduralDistributions.list.PoliceStorageAmmunition.items, "WeaponCache");
-	table.insert(ProceduralDistributions.list.PoliceStorageAmmunition.items, 0.002);
+	table.insert(ProceduralDistributions.list.PoliceStorageAmmunition.items, 0.0001);
 
+	table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "WeaponCache");
+	table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 0.00001);
+	
+	table.insert(ProceduralDistributions.list.GunStoreShelf.items, "WeaponCache");
+	table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.001);
+
+	-------------------------------------------------------------------------------------
 	table.insert(ProceduralDistributions.list.GarageMechanics.items, "MechanicCache");
 	table.insert(ProceduralDistributions.list.GarageMechanics.items, 0.001);
 
@@ -117,6 +101,7 @@ local function preDistributionMerge()
 	table.insert(ProceduralDistributions.list.CrateMechanics.items, "MechanicCache");
 	table.insert(ProceduralDistributions.list.CrateMechanics.items, 0.001);
 	
+	-------------------------------------------------------------------------------------
 	table.insert(ProceduralDistributions.list.GarageMetalwork.items, "MetalworkCache");
 	table.insert(ProceduralDistributions.list.GarageMetalwork.items, 0.001);
 
@@ -126,7 +111,7 @@ local function preDistributionMerge()
 	table.insert(ProceduralDistributions.list.CrateMetalwork.items, "MetalworkCache");
 	table.insert(ProceduralDistributions.list.CrateMetalwork.items, 0.001);
 	
-	
+	----------------------------------------------------------------------------------------
 	table.insert(ProceduralDistributions.list.GarageMechanics.items, "FarmerCache");
 	table.insert(ProceduralDistributions.list.GarageMechanics.items, 0.001);
 
@@ -147,6 +132,32 @@ local function preDistributionMerge()
 
 	table.insert(ProceduralDistributions.list.CrateMetalwork.items, "FarmerCache");
 	table.insert(ProceduralDistributions.list.CrateMetalwork.items, 0.001);
+	
+	----------------------------------------------------------------------------------------
+	table.insert(ProceduralDistributions.list.PoliceStorageAmmunition.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.PoliceStorageAmmunition.items, 0.001);
+	
+	table.insert(ProceduralDistributions.list.GunStoreAmmunition.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.GunStoreAmmunition.items, 0.001);
+	
+	table.insert(ProceduralDistributions.list.ArmyStorageAmmunition.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.ArmyStorageAmmunition.items, 0.001);
+	
+	table.insert(ProceduralDistributions.list.GunStoreCounter.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.GunStoreCounter.items, 0.0001);
+	
+	table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.GunStoreDisplayCase.items, 0.0001);
+	
+	table.insert(ProceduralDistributions.list.GunStoreShelf.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.0001);
+	
+	table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.PoliceStorageGuns.items, 0.0001);
+	
+	table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, "AmmoCache");
+	table.insert(ProceduralDistributions.list.ArmyStorageGuns.items, 0.0001);
+	
 end
 Events.OnPreDistributionMerge.Add(preDistributionMerge)
 
