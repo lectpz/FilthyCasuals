8231e11b815961a6d32aa7b72ca482f34918de02
lectpz <tohuang@gmail.com>
2023-12-24 11:39:07 -0500

    1.01 20231224
    fixed onweaponswing to not trigger when empty handed
    fixed grab buttons on inventory window pane
    removed safehousecheck

--- FCtweaks/media/lua/client/FCteleport.lua
+++ FCtweaks/media/lua/client/FCteleport.lua
@@ -105,13 +105,18 @@ local function FCteleportercontext(player, context, items) -- # When an inventor
 	items = ISInventoryPane.getActualItems(items); -- Get table of inventory items (will not be module.item, just item)
 	for _, item in ipairs(items) do -- Check every item in inventory array
 		if item:getFullType() == 'Base.WeaponCache' or item:getFullType() == 'Base.FarmerCache' or item:getFullType() == 'Base.MechanicCache' or item:getFullType() == 'Base.MetalworkCache' or item:getFullType() == 'Base.AmmoCache' then
-			print(item:getFullType())
+			--print(item:getFullType())
 			context:removeOptionByName(getText("ContextMenu_Grab"))
+			context:removeOptionByName(getText("ContextMenu_Grab_one"))
+			context:removeOptionByName(getText("ContextMenu_Grab_half"))
+			context:removeOptionByName(getText("ContextMenu_Grab_all"))
 			context:removeOptionByName(getText("ContextMenu_PutInContainer"))
 			context:removeOptionByName(getText("ContextMenu_Equip_Primary"))--'Equip Primary')
 			context:removeOptionByName(getText("ContextMenu_Equip_Secondary"))
 			context:removeOptionByName(getText("ContextMenu_Drop"))
 			context:removeOptionByName(getText("ContextMenu_PlaceItemOnGround"))
+			--ISInventoryPane:hideButtons()
+
 			--print(context:getMenuOptionNames())
 			
 			--function printTable(tbl)
---
+++ FCtweaks/media/lua/client/client/New folder/FCteleporttimedaction.lua
@@ -0,0 +1,51 @@
+require "TimedActions/ISBaseTimedAction"
+
+doFCteleport = ISBaseTimedAction:derive("doFCteleport");
+
+function doFCteleport:isValid()
+	return true;
+end
+
+function doFCteleport:waitToStart()
+    return false;
+end
+
+function doFCteleport:update()
+end
+
+function doFCteleport:start()
+	getSoundManager():PlayWorldSound("s_teleport", false, self.character:getCurrentSquare(), 30, 3, 10, false) ;
+	local soundRadius = 3
+	local volume = 6
+	addSound(self.character,
+				self.character:getX(),
+				self.character:getY(),
+				self.character:getZ(),
+				soundRadius,
+				volume)
+end
+
+function doFCteleport:stop()
+	getSoundManager():stop();
+	ISBaseTimedAction.stop(self);
+end
+
+function doFCteleport:perform()
+	ISBaseTimedAction.perform(self);
+end
+
+function doFCteleport:new(character, command, args)
+	local o = {}
+	setmetatable(o, self)
+	self.__index = self
+    o.character = character
+    o.command = command
+    o.args = args
+	o.gameSound = 0
+	o.maxTime = 1
+	o.stopOnWalk = true
+	o.stopOnRun = true
+	o.forceProgressBar = true
+	if o.character:isTimedActionInstant() then o.maxTime = 1; end
+	return o;
+end
