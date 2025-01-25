a93efa5b1ca609dd981c2ce43c54986efceb4be6
Brandon Korenek <brandonkorenek@gmail.com>
2025-01-25 15:48:12 -0600

    Updating luck display value and format for soul thist

--- SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewelryBuffs.lua
+++ SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewelryBuffs.lua
@@ -13,8 +13,8 @@ BuffSystem.BUFF_CALCULATIONS = {
     },
     luck = {
         format = "Luck",
-        getDisplayValue = function(tier) return 1 * tier end,
-        getBonus = function(tier) return 1 * tier end,
+        getDisplayValue = function(tier) return 1.5 * tier end,
+        getBonus = function(tier) return 1.5 * tier end,
         modData = "PermaSoulForgeLuckBonus"
     },
     SoulDexterity = {
@@ -24,7 +24,7 @@ BuffSystem.BUFF_CALCULATIONS = {
         modData = "PermaSoulForgeDexterityBonus"
     },
     SoulThirst = {
-        format = "Soul Thirst Bonus",
+        format = "Soul Thirst Bonus Chance",
         getDisplayValue = function(tier) return 4 * tier end,
         getBonus = function(tier) return 0.4 * tier end,
         modData = "PermaSoulThirstValue"
