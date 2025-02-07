ec9d42b6285110a12567e7ad3f6c6340fcaf8fb9
Brandon Korenek <brandonkorenek@gmail.com>
2025-02-07 09:55:50 -0600

    Adding max luck in tier 1

--- SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewelryOnZombieDead.lua
+++ SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewelryOnZombieDead.lua
@@ -13,11 +13,11 @@ end
 
 Events.OnZombieDead.Add(function(zombie)
     local tierZone = checkZone()
-	local pMD = getSpecificPlayer(0):getModData()
-    local luck = math.min(math.max((pMD.luckValue or 0) + (pMD.PermaSoulForgeLuckBonus or 0), 0), 150)
+    local pMD = getSpecificPlayer(0):getModData()
+    local maxLuck = tierZone == 1 and 75 or 150
+    local luck = math.min(math.max((pMD.luckValue or 0) + (pMD.PermaSoulForgeLuckBonus or 0), 0), maxLuck)
 
-
-    if ZombRand(tierZone*150 - luck) == 0 and isValidZone(tierZone) then
+    if ZombRand(math.min(tierZone*150 - luck,1)) == 0 and isValidZone(tierZone) then
         local items = ItemGenerator.getTierSoulShard()
         local result = nil
         EventHandlers.SoulForgedJewelryOnCreate(items, result, zombie)
