e87d6b4543317bcdb0c45b2241292a0dfad555a3
lectpz <117684200+lectpz@users.noreply.github.com>
2025-02-07 11:21:16 -0500

    Update SoulForgedJewelryOnZombieDead.lua

--- SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewelryOnZombieDead.lua
+++ SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewelryOnZombieDead.lua
@@ -17,7 +17,7 @@ Events.OnZombieDead.Add(function(zombie)
     local maxLuck = tierZone == 1 and 75 or 150
     local luck = math.min(math.max((pMD.luckValue or 0) + (pMD.PermaSoulForgeLuckBonus or 0), 0), maxLuck)
 
-    if ZombRand(math.min(tierZone*150 - luck,1)) == 0 and isValidZone(tierZone) then
+    if ZombRand(math.max(tierZone*150 - luck,1)) == 0 and isValidZone(tierZone) then
         local items = ItemGenerator.getTierSoulShard()
         local result = nil
         EventHandlers.SoulForgedJewelryOnCreate(items, result, zombie)
