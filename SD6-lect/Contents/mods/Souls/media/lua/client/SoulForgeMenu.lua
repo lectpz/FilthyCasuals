018f9ac47d4291b304c2a480d9f13209a1da766c
Maria Alexis Sales <mariaalexissales@gmail.com>
2025-06-14 11:47:12 -0500

    Update SoulForgeMenu.lua

--- SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgeMenu.lua
+++ SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgeMenu.lua
@@ -1,3 +1,30 @@
--- Context menu for Soul Forgeable Weapons
+SoulForgeMenu = {}
 
--- TODO: Add soulForgeContextMenu
+local QualityTickets = require("SoulForgeQualityTickets")
+local TooltipUtils = require("SoulForgedTooltipUtils")
+local WeaponUtils = require("SoulForgedWeaponUtils")
+local Config = require("SoulForgedConfig")
+
+function SoulForgeMenu.addMenu(player, context, items)
+    local weapon = SoulForgeMenu.getSelectedWeapon(items)
+
+    if not weapon then return end
+
+    local qualityTickets = QualityTickets.scanInventory(getSpecificPlayer(player))
+
+    if not qualityTickets then return end
+    
+    SoulForgeMenu.buildQualityTicketMenu(player, context, weapon, qualityTickets)
+    -- Future features would need SoulForgeMenu.build(feature)Menu
+end
+
+function SoulForgeMenu.getSelectedWeapon(items)
+    for _, entry in ipairs(items) do
+        if instanceof(entry, "InventoryItem") and entry:IsWeapon() then
+            return entry
+        end
+    end
+end
+
+function SoulForgeMenu.buildQualityTicketMenu(player, context, weapon, qualityTickets)
+    local 
