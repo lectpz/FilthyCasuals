3d27a97457a79384d190d0c32b6b42bf8048ce42
Maria Alexis Sales <mariaalexissales@gmail.com>
2025-07-07 16:24:40 -0500

    Reverting Prettier change pt.2

--- SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewlery.lua
+++ SD6-lect/Contents/mods/Souls/media/lua/client/SoulForgedJewlery.lua
@@ -17,7 +17,7 @@ function OnTest_CheckInInventory(item)
 
     if isOwnSafeHouse then
         local shx1 = isOwnSafeHouse:getX()
-        local shy1 = isOwnSafeHouse:getY() - 15
+        local shy1 = isOwnSafeHouse:getY()-15
         local shx2 = isOwnSafeHouse:getW() + shx1
         local shy2 = isOwnSafeHouse:getH() + shy1
 
@@ -25,26 +25,26 @@ function OnTest_CheckInInventory(item)
             return true
         end
     end
-
+    
     if not item:isInPlayerInventory() then return false end
 
     return true
-end
+ end
 
--- Override transfer speed for dexterity buff
+ -- Override transfer speed for dexterity buff
 local original_new = ISInventoryTransferAction.new
 function ISInventoryTransferAction:new(character, item, srcContainer, destContainer, time)
     local o = original_new(self, character, item, srcContainer, destContainer, time)
     local dexterityBonus = character:getModData().PermaSoulForgeDexterityBonus or 0
-
+    
     if o and dexterityBonus > 0 then
         o.maxTime = o.maxTime - (o.maxTime * dexterityBonus)
     end
-
-    --print("old o.maxTime",o.maxTime)
-    if SDxferQOL then o.maxTime = o.maxTime * 0.25 end
-    --print("new o.maxTime",o.maxTime)
-
+		
+	--print("old o.maxTime",o.maxTime)
+	if SDxferQOL then o.maxTime = o.maxTime * 0.25 end
+	--print("new o.maxTime",o.maxTime)
+	
     return o
 end
 
@@ -52,8 +52,8 @@ SoulForgedJewelryOnCreate = EventHandlers.SoulForgedJewelryOnCreate
 function SoulForgedJewelryOnCreateCache(items, result, player)
     local cacheItem = items:get(0)
     local tier = tonumber(string.match(cacheItem:getType(), "T(%d+)"))
-
-    for i = 1, 3 do
+    
+    for i=1,3 do
         items = ItemGenerator.getTierSoulShardExplicit(tier)
         SoulForgedJewelryOnCreate(items, result, player)
     end
@@ -61,15 +61,15 @@ end
 
 function getSoulForgedValidItems()
     local validItems = ItemGenerator.getValidItems()
-
+    
     print("Valid Soulforged Items:")
     print("----------------------")
-
+    
     table.sort(validItems)
-
+    
     for i, itemName in ipairs(validItems) do
         print(string.format("%d. %s", i, itemName))
     end
-
+    
     print(string.format("\nTotal valid items: %d", #validItems))
 end
